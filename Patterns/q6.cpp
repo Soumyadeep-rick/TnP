4a06cfc37ca9a31f09d7754d117382fc1a8e642d
Soumyadeep Mandal <1906215@kiit.ac.in>
2021-09-26 00:20:16 +0530

    New patterns added

--- Patterns/q6.cpp
+++ Patterns/q6.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n, k = 1;
     cout << "n: ";
     cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            int l = k + i;
+            k = (k + i + 1);
+            for (int j = 0; j < i - 1; j++)
+            {
+                cout << l << "*";
+                l--;
+            }
+            cout << l << endl;
+        }
+        else
+        {
+            for (int j = 0; j < i - 1; j++)
+            {
+                cout << k << "*";
+                k++;
+            }
+            cout << k << endl;
+        }
+    }
 }
